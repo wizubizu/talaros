bed8dc1f979594875d8cd1a4d5eecafc429aec39
wizubizu <odugbesanabimbola@gmail.com>
2018-07-30 09:22:01 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,66 +25,14 @@ var app = {
 
 }
 
-var apps = {
- imageHandler:function() {
-        $("#upload1").on('change', function() {
-            //alert("alert");
-            readUrl();
-        });
-        function readUrl (){
-            var file = $("#upload1")[0].files[0];
-            //console.log(file);
-            //reader interprets the file selected
-            var reader = new FileReader();
-                //console.log(reader.result);
-            reader.onloadend = function (){
-                //console.log(reader.result);
-                $("#picture_preview1").attr("src", reader.result);
-                //$("#picture_preview")[0].src = "" + reader.results;
-                //console.log($("#picture_preview"));
-                //console.log(reader.result);
-            }
-            if(file){
-                reader.readAsDataURL(file);
-            }
-        }
-    }//end of imageHandler
-    
-}
 
-var appz = {
- imageHandler:function() {
-        $("#upload2").on('change', function() {
-            //alert("alert");
-            readUrl();
-        });
-        function readUrl (){
-            var file = $("#upload2")[0].files[0];
-            //console.log(file);
-            //reader interprets the file selected
-            var reader = new FileReader();
-                //console.log(reader.result);
-            reader.onloadend = function (){
-                //console.log(reader.result);
-                $("#picture_preview2").attr("src", reader.result);
-                //$("#picture_preview")[0].src = "" + reader.results;
-                //console.log($("#picture_preview"));
-                //console.log(reader.result);
-            }
-            if(file){
-                reader.readAsDataURL(file);
-            }
-        }
-    }//end of imageHandler
-    
-}
 
 
 
 
  
 
-// vapo js///
+
     app.imageHandler();
 
     $("[data-toggle=popover]").popover({
@@ -94,55 +42,33 @@ var appz = {
         }
     });
 
-    // dropdown
-    $(document).ready(function(){
-        $('.dropdown-submenu a.test').on("click", function(e){
-            $(this).next('ul').toggle();
-            e.stopPropagation();
-            e.preventDefault();
-        });
-    });
-
-    // when an input is active button show
-    $(".__inputactiv").keyup(function () {
-       if ($(this).val()) {
-          $(".__btnshow").show();
-       }
-       else {
-          $(".__btnshow").hide();
-       }
-    });
-    $(".__btnshow").click(function () {
-       $(".__inputactiv").val('');
-       $(this).hide();
-    });
+    
 
 
 
 
     // accordion
+    var acc = document.getElementsByClassName("__accordion");
+    var i;
 
-    $(document).ready(function() {
-    $('.__accordion').find('.accordion-toggle').click(function() {
-        $(this).next().slideToggle('600');
-        $(".accordion-content").not($(this).next()).slideUp('600');
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+            panel.style.maxHeight = null;
+        } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            } 
         });
-        $('.accordion-toggle').on('click', function() {
-            $(this).toggleClass('active').siblings().removeClass('active');
-        });
-    });
+    }
 
 
     
 
     // end
 
-    //fileupload for both
-        $('input[id=base-input5]').change(function() {
-            $('#fake-input5').val($(this).val().replace("C:\\fakepath\\", ""));
-        });
-
-        // typed writer
+    // typed writer
 
         document.addEventListener('DOMContentLoaded', function(){
 
@@ -210,57 +136,10 @@ var appz = {
 
 
 
-// show another navbar menu when scroll down
 
-    var myID = document.getElementById("myID");
-
-    var myScrollFunc = function() {
-      var y = window.scrollY;
-      if (y >= 50) {
-        myID.className ="__bottomMenu show"
-      } else {
-        myID.className ="__bottomMenu hide"
-      }
-    };
-
-    window.addEventListener("scroll", myScrollFunc);
-
-
-    // dashboard show and hide
-    $('#openSidebar').click(function(){
-        $('.leftbar').addClass('biger');
-        });
-    $('#closebtn').click(function(){
-        $('.leftbar').removeClass('biger');
-    });
-    // dashboard for traveler
-    $('#openSidebar').click(function(){
-        $('.leftbart').addClass('biger');
-        });
-    $('#closebtn').click(function(){
-        $('.leftbart').removeClass('biger');
-    });
-
-     // dashboard show and hide
-    $('#openrightSidebars').click(function(){
-        $('.rightbars').addClass('bigers');
-        });
-    $('#rightclosebtns').click(function(){
-        $('.rightbars').removeClass(' bigers');
-    });
     
 
 
-
-// preview and hide password
-    $("#showPassword").click(function(){
-      var foo = $(this).prev().attr("type");
-      if(foo == "password"){
-        $(this).prev().attr("type", "text");
-      } else {
-        $(this).prev().attr("type", "password");
-      }
-    });
 
 
 
@@ -340,21 +219,7 @@ var appz = {
    
     };
 
-    // accordion plus sign
-     var acc = document.getElementsByClassName("__accordion1");
-        var i;
-
-        for (i = 0; i < acc.length; i++) {
-            acc[i].addEventListener("click", function() {
-            this.classList.toggle("__active");
-            var panel = this.nextElementSibling;
-            if (panel.style.maxHeight){
-                panel.style.maxHeight = null;
-            } else {
-                    panel.style.maxHeight = panel.scrollHeight + "px";
-                } 
-            });
-        }
+   
 
 
         //only number
@@ -535,130 +400,9 @@ var appz = {
      $('#timepicker1').timepicki();
      // end
 
-     //modal button and star active change bgcolor
-        $(".__actbtn, .__starm").click(function() {
-          $(this).toggleClass('foo');
-        });
-
-        $(".__actinbtn").click(function() {
-          $(this).toggleClass('foo');
-        });
-
-        $(".__actin1btn").click(function() {
-          $(this).toggleClass('foo');
-        });
-    // end
-
      
 
-
-
-    // traveller file upload
-
-    
-    var fileTypes = ['pdf', 'docx', 'rtf', 'jpg', 'jpeg', 'png', 'txt'];  //acceptable file types
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-            var extension = input.files[0].name.split('.').pop().toLowerCase(),  //file extension from input file
-                isSuccess = fileTypes.indexOf(extension) > -1;  //is extension in acceptable types
-
-            if (isSuccess) { //yes
-                var reader = new FileReader();
-                reader.onload = function (e) {
-                    if (extension == 'pdf'){
-                        $(input).closest('.fileUpload').find(".icon").attr('src','https://image.flaticon.com/icons/svg/179/179483.svg');
-                    }
-                    else if (extension == 'docx'){
-                        $(input).closest('.fileUpload').find(".icon").attr('src','https://image.flaticon.com/icons/svg/281/281760.svg');
-                    }
-                    else if (extension == 'rtf'){
-                        $(input).closest('.fileUpload').find(".icon").attr('src','https://image.flaticon.com/icons/svg/136/136539.svg');
-                    }
-                    else if (extension == 'png'){ $(input).closest('.fileUpload').find(".icon").attr('src','https://image.flaticon.com/icons/svg/136/136523.svg'); 
-                    }
-                    else if (extension == 'jpg' || extension == 'jpeg'){
-                        $(input).closest('.fileUpload').find(".icon").attr('src','https://image.flaticon.com/icons/svg/136/136524.svg');
-                    }
-                  else if (extension == 'txt'){
-                        $(input).closest('.fileUpload').find(".icon").attr('src','https://image.flaticon.com/icons/svg/136/136538.svg');
-                    }
-                    else {
-                        //console.log('here=>'+$(input).closest('.uploadDoc').length);
-                        $(input).closest('.uploadDoc').find(".docErr").slideUp('slow');
-                    }
-                }
-
-                reader.readAsDataURL(input.files[0]);
-            }
-            else {
-                    //console.log('here=>'+$(input).closest('.uploadDoc').find(".docErr").length);
-                $(input).closest('.uploadDoc').find(".docErr").fadeIn();
-                setTimeout(function() {
-                        $('.docErr').fadeOut('slow');
-                        }, 9000);
-            }
-        }
-    }
-    $(document).ready(function(){
-       
-       $(document).on('change','.up', function(){
-        var id = $(this).attr('id'); /* gets the filepath and filename from the input */
-           var profilePicValue = $(this).val();
-           var fileNameStart = profilePicValue.lastIndexOf('\\'); /* finds the end of the filepath */
-           profilePicValue = profilePicValue.substr(fileNameStart + 1).substring(0,20); /* isolates the filename */
-           //var profilePicLabelText = $(".upl"); /* finds the label text */
-           if (profilePicValue != '') {
-            //console.log($(this).closest('.fileUpload').find('.upl').length);
-              $(this).closest('.fileUpload').find('.upl').html(profilePicValue); /* changes the label text */
-           }
-       });
-
-       $(".btn-new").on('click',function(){
-            $("#uploader").append('<div class="row uploadDoc"><div class="col-sm-11 col-xs-10"><div class="docErr">Please upload valid file</div><!--error--><div class="fileUpload btn btn-orange"> <img src="https://image.flaticon.com/icons/svg/136/136549.svg" class="icon"><span class="upl" id="upload">Upload document</span><input type="file" class="upload up" id="up" onchange="readURL(this);" /></div></div><div class="col-sm-1 col-xs-2"><a class="btn-check"><i class="fa fa-times"></i></a></div></div>');
-       });
-        
-       $(document).on("click", "a.btn-check" , function() {
-         if($(".uploadDoc").length>1){
-            $(this).closest(".uploadDoc").remove();
-          }else{
-            alert("You have to upload at least one document.");
-          } 
-       });
-    });
-    // end
-
-    // remove table
-    $('.delete').on('click', function(event) {
-    $(this).parents('tr').remove();
-    });
-
-    $(function () {
-        $(".deleteBox .delete").click(function(){
-            $(this).parents(".deleteBox").animate({ opacity: "hide" }, "slow"); 
-        });
-    });
-
-    
-    // end
-
-    // download PDF
-    var doc = new jsPDF();
-    var specialElementHandlers = {
-        '#editor': function (element, renderer) {
-        return true;
-        }
-    };
-
-    $('#cmd').click(function () {
-        doc.fromHTML($('#content').html(), 15, 15, {
-            'width': 170,
-            'elementHandlers': specialElementHandlers
-        });
-        doc.save('Visa guide.pdf');
-    });
-    
-
-
+     
 
 
 
